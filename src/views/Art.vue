e0824c8af0f1fcc21f8073473ec3a46b70e35123
Francesca Losavio <lo.francesca97@gmail.com>
2021-03-07 18:46:06 +0100

    implement masonry layout

--- src/views/Art.vue
+++ src/views/Art.vue
@@ -2,8 +2,10 @@
   <div class="home">
     <div id="image-container">
 
-      <div v-for="image in images" :key="image.src">
-        <img :src="image.src" alt="" class="art-image">
+      <div v-for="(column, index) in imageColumns" :key="index" class="column">
+        <div v-for="image in column" :key="image.src">
+          <img :src="image.src" alt="" class="art-image">
+        </div>
       </div>
 
     </div>
@@ -14,11 +16,11 @@
   #image-container {
     display: flex;
     flex-direction: row;
-    flex-wrap: wrap;
+    justify-content: center;
 
     margin: auto;
     width: 800px;
-    background: green;
+    /* background: green; */
   }
 
   .art-image {
@@ -33,20 +35,33 @@ export default {
   data () {
     return {
       images: [
-        { src: require('../assets/art/ball_du_monde.png') },
-        { src: require('../assets/art/cave_kabarett.png') },
-        { src: require('../assets/art/caveman_podcast.png') },
-        { src: require('../assets/art/dirty_dancing.png') },
-        { src: require('../assets/art/ex_libris.jpg') },
-        { src: require('../assets/art/kleiderkreisel.png') },
-        { src: require('../assets/art/letter_f.jpg') },
-        { src: require('../assets/art/martin_face.png') },
-        { src: require('../assets/art/nathaly_benni.png') },
-        { src: require('../assets/art/owl_fish.png') },
-        { src: require('../assets/art/papillonia.png') },
-        { src: require('../assets/art/spittelau.jpg') },
-        { src: require('../assets/art/strangers_garden.png') }
+        { src: require('../assets/art/nathaly_benni.png'), col: 0 },
+        { src: require('../assets/art/ball_du_monde.png'), col: 0 },
+        { src: require('../assets/art/kleiderkreisel.png'), col: 0 },
+        { src: require('../assets/art/martin_face.png'), col: 0 },
+        { src: require('../assets/art/strangers_garden.png'), col: 1 },
+        { src: require('../assets/art/spittelau.jpg'), col: 1 },
+        { src: require('../assets/art/caveman_podcast.png'), col: 1 },
+        { src: require('../assets/art/letter_f.jpg'), col: 1 },
+        { src: require('../assets/art/cave_kabarett.png'), col: 2 },
+        { src: require('../assets/art/dirty_dancing.png'), col: 2 },
+        { src: require('../assets/art/papillonia.png'), col: 2 },
+        { src: require('../assets/art/owl_fish.png'), col: 2 }
+        // { src: require('../assets/art/ex_libris.jpg'), col: 2 },
       ]
+    }
+  },
+  computed: {
+    imageColumns: function () {
+      // get all column numbers (without duplicates)
+      const existingColumns = [...new Set(this.images.map(e => e.col))]
+
+      // sort column numbers ascending
+      existingColumns.sort((a, b) => a > b ? 1 : -1)
+
+      // get image data for each column
+      const columns = existingColumns.map(number => this.images.filter(image => image.col === number))
+      return columns
     }
   }
 }
